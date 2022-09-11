9ddcb5a7e3ccff90f9d465afb4507c7e2380983d
yemiancheng <ymc.github@gmail.com>
2022-09-11 09:32:37 +0800

    docs(core): update usage comment in instance
    with xx to option.xx
    
    generated by ymc@robot

--- packages/read-diretory/src/index-ins.js
+++ packages/read-diretory/src/index-ins.js
@@ -5,6 +5,7 @@ import { log, blue } from './index-too.js'
 let option = {
   excludes: ['dist', 'lib', 'libtpl-rollup-plugins', 'vagrant']
 }
+//docs: update usage comment in instance\nwith xx to option.xx
 // log(blue(`get dst dir: node_modules`))
 // getDstDir("D:\\code-store",/node_modules/ig)
 
@@ -12,38 +13,38 @@ let option = {
 // getDstDir("F:\\bilibili-dance",/ +/ig)
 
 // log(blue(`get dst file: the file that name with copy and ends with .js`))
-// DST_MODE='file'
+// option.mode='file'
 // getDstDir("D:\\code-store",/copy.*\.js$/ig)
 
 // log(blue(`get dst file: the file that name with fav and ends with .js`))
-// DST_MODE='file'
+// option.mode='file'
 // getDstDir("D:\\code-store",/fav.*\.js$/ig)
 
 log(blue(`get dst file: the file that name with hua and ends with .sh`))
-// DST_MODE = 'file'
+// option.mode = 'file'
 option.regexp = /hzh.*\.sh$/gi
 getDstDir('D:\\code-store', option)
 
 // log(blue(`get dst file: the file that file text with ycs and name ends with .js`))
-// DST_MODE='file_text'
-// DST_FILE_REGEXP=/ycs/ig
+// option.mode='file_text'
+// option.fileTextRegexp=/ycs/ig
 // getDstDir("H:\\js-project",/\.js$/ig)
 
 // log(blue(`get dst file: the file that file text with ycs and name ends with .js`))
-// DST_MODE='file_text'
-// DST_FILE_REGEXP=/curd-dirs-sync/ig
+// option.mode='file_text'
+// option.fileTextRegexp=/curd-dirs-sync/ig
 // getDstDir("H:\\js-project",/\.js$/ig)
 
 // log(blue(`get dst file: the file that file text with jsdoc and name ends with .js`))
-// DST_MODE='file_text'
-// DST_FILE_REGEXP=/jsdoc/ig
-// IGNORE_DIR_REGEXP=/node_modules/ig
+// option.mode='file_text'
+// option.fileTextRegexp=/jsdoc/ig
+// option.excludesRegexp=/node_modules/ig
 // // getDstDir("H:\\js-project",/\.js$/ig)
 // getDstDir("D:\\code-store\\typescript",/\.js$/ig)
 
 // log(blue(`get dst file: the file that file text with eslint and name ends with .js`))
-// DST_MODE='file_text'
-// DST_FILE_REGEXP=/eslint/ig
-// IGNORE_DIR_REGEXP=/node_modules/ig
+// option.mode='file_text'
+// option.fileTextRegexp=/eslint/ig
+// option.excludesRegexp=/node_modules/ig
 // getDstDir("D:\\code-store\\typescript",/\.js$/ig)
 // node packages/read-diretory/src/index-ins.js

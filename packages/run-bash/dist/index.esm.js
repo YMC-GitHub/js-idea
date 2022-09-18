ffeab09545b48ebc1b12bfae4fa6edf2a953b9da
yemiancheng <ymc.github@gmail.com>
2022-09-18 23:25:58 +0800

    feat(core): fix unreadable zh code
    with a handle at execOpts.fixUnreadbleCode
    
    generated by ymc@robot

--- packages/run-bash/dist/index.esm.js
+++ packages/run-bash/dist/index.esm.js
@@ -16,15 +16,15 @@ import { exec } from 'child_process';
  * @returns {string}
  */
 const cmdOptArr2cmdOptStr = (cmdOptStr, splitChar = ' ') => {
-  return Array.isArray(cmdOptStr) ? cmdOptStr.join(splitChar) : cmdOptStr
+    return Array.isArray(cmdOptStr) ? cmdOptStr.join(splitChar) : cmdOptStr
 };
 
 function trimstdout(stdout) {
-  return stdout
-    .split(/\r?\n/)
-    .map(v => v.trim())
-    .filter(v => v)
-    .join('\n')
+    return stdout
+        .split(/\r?\n/)
+        .map(v => v.trim())
+        .filter(v => v)
+        .join('\n')
 }
 /**
  * exec wraper
@@ -40,52 +40,61 @@ function trimstdout(stdout) {
  * ```
  */
 const execWraper = (cmd, cmdOpts, execOpts) => {
-  return new Promise((resolve, reject) => {
-    //desc: for exec(`git --version`],execOpts)
-    if (!execOpts) {
-      execOpts = cmdOpts;
-      cmdOpts = cmd;
-      cmd = '';
-    }
-
-    const option = cmdOptArr2cmdOptStr(cmdOpts); //desc: other yuyi to string
-    // let { exec } = execOpts //eg:{exec}=require("child_process");
-    //fix: exec is optional in execOpts
-    let run = execOpts.exec ? execOpts.exec : exec;
-    cmd = cmd ? `${cmd} ${option}` : `${option}`;
-    // cmd=`${cmd} ${option}`.trimStart()
-
-    //delete execOpts.exec; //desc:clean some property to keep execOpts as native
-
-    //support exe opt : exec(cmd,execOpts,callback)
-    //https://stackoverflow.com/questions/18894433/nodejs-child-process-working-directory
-    run(`${cmd}`, execOpts, (e, stdout, stderr) => {
-      //feat: set reject err to be optional\nwhen execOpts.exitWhenErr=true
-      if (e && execOpts.exitWhenErr) {
-        reject(e);
-      }
-      //feat(core): trim stdout and stderr \ndo not trim when execOpts.noTrimOut=true
-      if (!execOpts.noTrimOut) {
-        stdout = trimstdout(stdout);
-        stderr = trimstdout(stderr);
-      }
-
-      //case:reject std err and resolve std res
-      //feat(core): set reject stderr to be optional in execOpts\nreject when execOpts.rejectStderr=true
-      if (execOpts.rejectStderr) {
-        if (stderr) {
-          reject(e);
+    return new Promise((resolve, reject) => {
+        //desc: for exec(`git --version`],execOpts)
+        if (!execOpts) {
+            execOpts = cmdOpts;
+            cmdOpts = cmd;
+            cmd = '';
         }
-        resolve(stdout);
-      }
-      //case:resolve std err and res
-      resolve({ stdout, stderr });
-    });
-  })
+
+        const option = cmdOptArr2cmdOptStr(cmdOpts); //desc: other yuyi to string
+        // let { exec } = execOpts //eg:{exec}=require("child_process");
+        //fix: exec is optional in execOpts
+        let run = execOpts.exec ? execOpts.exec : exec;
+        cmd = cmd ? `${cmd} ${option}` : `${option}`;
+        // cmd=`${cmd} ${option}`.trimStart()
+
+        //delete execOpts.exec; //desc:clean some property to keep execOpts as native
+
+        //support exe opt : exec(cmd,execOpts,callback)
+        //https://stackoverflow.com/questions/18894433/nodejs-child-process-working-directory
+        run(`${cmd}`, execOpts, (e, stdout, stderr) => {
+            //feat:fix unreadable zh code\with option.fixUnreadbleCode
+            let { fixUnreadbleCode } = execOpts;
+            if (fixUnreadbleCode) {
+                //fixUnreadbleCode=(code,charset="cp936")=>{return iconv.decode(err, charset)})
+                fixUnreadbleCode(e);
+                fixUnreadbleCode(stdout);
+                fixUnreadbleCode(stderr);
+            }
+
+            //feat: set reject err to be optional\nwhen execOpts.exitWhenErr=true
+            if (e && execOpts.exitWhenErr) {
+                reject(e);
+            }
+            //feat(core): trim stdout and stderr \ndo not trim when execOpts.noTrimOut=true
+            if (!execOpts.noTrimOut) {
+                stdout = trimstdout(stdout);
+                stderr = trimstdout(stderr);
+            }
+
+            //case:reject std err and resolve std res
+            //feat(core): set reject stderr to be optional in execOpts\nreject when execOpts.rejectStderr=true
+            if (execOpts.rejectStderr) {
+                if (stderr) {
+                    reject(e);
+                }
+                resolve(stdout);
+            }
+            //case:resolve std err and res
+            resolve({ stdout, stderr });
+        });
+    })
 };
 
 const execOpts = {
-  exec: exec
+    exec: exec
 };
 
 export { execWraper as exec, execOpts };

e8e74861b5980c42bbc912d0cce6edd010859df6
yemiancheng <ymc.github@gmail.com>
2022-09-19 21:26:05 +0800

    feat(core): export help function
    with defFixUnreadbleCode
    
    generated by ymc@robot

--- packages/run-bash/src/index.js
+++ packages/run-bash/src/index.js
@@ -1,6 +1,6 @@
 import { exec as cp_exec } from 'child_process'
 
-export { execWraper as exec } from './util.js'
+export { execWraper as exec, defFixUnreadbleCode } from './util.js'
 
 export const execOpts = {
     exec: cp_exec

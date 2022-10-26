f3602e9daba01cfb5be5d392dfee20d4c18d2b75
yemiancheng <ymc.github@gmail.com>
2022-10-26 16:11:54 +0800

    feat(core): speed up github by url
    add prefix url to matched url
    
    generated by ymc@robot

---
+++ packages/speed-up-github-by-url/dist/index.esm.js
@@ -0,0 +1,58 @@
+/**
+  * speedUpGithubByUrl v1.0.0
+  * (c) 2018-2022 ymc
+  * @license MIT
+  */
+/* eslint-disable no-use-before-define */
+// fix infoProxyDomainMap' was used before it was defined
+const { log } = console;
+
+/** @typedef {[string,string][]} proxyDomainMap */
+
+// pkg:
+// @ymc/speed-up-github-by-url
+/**
+ * speedup github - add prefix url to matched url
+ * @param {string} url
+ * @param {{map?:proxyDomainMap,infoProxy:boolean}} option
+ * @returns {string}
+ */
+function speedUpGithubByUrl(url, option = {}) {
+  let { map } = option;
+  if (!map) {
+    map = [
+      ['https://github.com', 'https://hub.fastgit.xyz'],
+      ['https://raw.githubusercontent.com', 'https://raw.fastgit.org']
+    ];
+  }
+  if (option.infoProxy) {
+    log('[info] add prefix to matched url');
+    infoProxyDomainMap(map);
+  }
+
+  // add prefix url when matched url
+  let res = url;
+  if (res) {
+    map.forEach(item => {
+      const [doamin, proxyByDomain] = item;
+      res = res.replace(new RegExp(`^${doamin}`, 'ig'), proxyByDomain);
+    });
+  }
+  return res
+}
+
+/**
+ *
+ * @param {proxyDomainMap} map
+ */
+function infoProxyDomainMap(map) {
+  const res = map
+    .map(v => {
+      const [domain, proxyByDomain] = v;
+      return `proxy:${proxyByDomain} domain:${domain}`
+    })
+    .join('\n');
+  log(res);
+}
+
+export { infoProxyDomainMap as infoProxy, speedUpGithubByUrl as speedup };
---
+++ packages/speed-up-github-by-url/readme.md
@@ -0,0 +1,18 @@
+# pkg speed-up-github-by-url
+
+## desc
+speedup github - add prefix url to matched url
+
+## lib size  
+file | size | gzip | brotli
+:---- | :---- | :---- | :----
+dist/index.js | 1.83kb | 0.82kb | 0.73kb
+dist/index.min.js | 0.81kb | 0.51kb | 0.45kb
+dist/index.esm.js | 1.32kb | 0.63kb | 0.57kb
+dist/index.esm.min.js | 0.52kb | 0.37kb | 0.32kb
+
+## Author
+yemiancheng <ymc.github@gmail.com>
+
+## License
+MIT

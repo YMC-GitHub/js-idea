ce1c0d711f2fe59f196ab7173dbd5548264dbf46
yemiancheng <ymc.github@gmail.com>
2022-10-26 17:05:43 +0800

    feat(core): chain async tasks
    export handle as default
    
    generated by ymc@robot

---
+++ packages/chain-task/dist/index.cjs.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/**
+ * chain async task
+ * @param {()=>{}} tasks
+ * @returns {{}[]}
+ */
+async function chaintask(tasks) {
+  const res = [];
+  let chain = Promise.resolve(null); // fix Unary operator '++' used       no-plusplus
+
+  /* eslint-disable no-plusplus */
+
+  for (let index = 0; index < tasks.length; index++) {
+    const task = tasks[index]; // fix Unexpected console statement   no-console
+    // fix 'v' is defined but never used  no-unused-vars
+
+    /* eslint-disable no-unused-vars,no-console */
+
+    chain = chain.then(async v => {
+      // feat: save each result to res
+      res[index] = await task();
+      return res[index];
+    }).catch(console.log);
+  }
+
+  await chain;
+  return res;
+}
+
+module.exports = chaintask;
---
+++ packages/chain-task/readme.md
@@ -0,0 +1,18 @@
+# pkg chain-task
+
+## desc
+chain async task
+
+## lib size  
+file | size | gzip | brotli
+:---- | :---- | :---- | :----
+dist/index.js | 1.14kb | 0.57kb | 0.48kb
+dist/index.min.js | 0.48kb | 0.33kb | 0.27kb
+dist/index.esm.js | 0.79kb | 0.43kb | 0.36kb
+dist/index.esm.min.js | 0.26kb | 0.22kb | 0.18kb
+
+## Author
+yemiancheng <ymc.github@gmail.com>
+
+## License
+MIT

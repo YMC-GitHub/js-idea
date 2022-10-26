75470a4a29d881617e2ad8449a6f0218872ca0a9
yemiancheng <ymc.github@gmail.com>
2022-10-26 14:43:32 +0800

    feat(core): add lib to extend date
    export extendDatePrototype
    expot formatDate
    
    generated by ymc@robot

---
+++ packages/extend-date/dist/index.esm.js
@@ -0,0 +1,87 @@
+/**
+  * extendDate v1.0.0
+  * (c) 2018-2022 ymc
+  * @license MIT
+  */
+/**
+ * format date
+ * @param {string} fmt
+ * @returns {string}
+ * @sample
+ * ```
+ * let now = new Date();
+ * formatDate("yyyy-MM-dd HH:mm:ss",now);
+ * ```
+ * @description
+ * ```
+ * M+
+ * ```
+ */
+function formatDate(fmt, ctx) {
+  // let ctx = this;
+  const o = {
+    'M+': ctx.getMonth() + 1,
+    'd+': ctx.getDate(),
+    'H+': ctx.getHours(),
+    'm+': ctx.getMinutes(),
+    's+': ctx.getSeconds(),
+    'S+': ctx.getMilliseconds()
+  };
+  // log(fmt, o);
+  // get year eg . formatDate('yyyy')//2022
+  let reg;
+  reg = /(y+)/;
+  if (reg.test(fmt)) {
+    // deprecated
+    // fmt = fmt.replace(
+    //   RegExp.$1,
+    //   `${ctx.getFullYear()}`.substr(4 - RegExp.$1.length)
+    // );
+    fmt = fmt.replace(reg, x => `${ctx.getFullYear()}`.substring(4 - x.length));
+  }
+  // add 0 before result with length
+  for (const k in o) {
+    reg = new RegExp(`(${k})`);
+    if (reg.test(fmt)) {
+      // deprecated
+      // fmt = fmt.replace(
+      //   RegExp.$1,
+      //   RegExp.$1.length == 1 ? o[k] : `00${o[k]}`.substr(String(o[k]).length)
+      // );
+      fmt = fmt.replace(reg, x => (x.length == 1 ? o[k] : `00${o[k]}`.substring(String(o[k]).length)));
+    }
+  }
+  return fmt
+}
+
+/**
+ *
+ * @param {string} name
+ * @param {()=>{}} handle
+ */
+function extendDatePrototype(name, handle) {
+  let tobeExtende = Date.prototype;
+  if (!tobeExtende[name]) {
+    tobeExtende[name] = function (...args) {
+      return handle(this, ...args)
+    };
+  }
+}
+
+/**
+ * format date
+ * @param {string} fmt
+ * @returns {string}
+ * @sample
+ * ```
+ * let now = new Date();
+ * now.Format("yyyy-MM-dd HH:mm:ss");
+ * ```
+ */
+// extendDatePrototype('formtDate',formatDate)
+extendDatePrototype('formatDate', formatDate);
+
+extendDatePrototype('Format', formatDate);
+// export default formatDate;
+
+export { extendDatePrototype, formatDate };
---
+++ packages/extend-date/readme.md
@@ -0,0 +1,18 @@
+# pkg extend-date
+
+## desc
+extend node.js date, extend Date.prototype is optional
+
+## lib size  
+file | size | gzip | brotli
+:---- | :---- | :---- | :----
+dist/index.js | 2.45kb | 0.94kb | 0.81kb
+dist/index.min.js | 0.88kb | 0.51kb | 0.44kb
+dist/index.esm.js | 1.84kb | 0.72kb | 0.63kb
+dist/index.esm.min.js | 0.60kb | 0.37kb | 0.32kb
+
+## Author
+yemiancheng <ymc.github@gmail.com>
+
+## License
+MIT

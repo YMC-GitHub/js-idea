7e8de59e890d3a4352741e2d08e3e30f6415ac82
yemiancheng <ymc.github@gmail.com>
2022-10-28 23:31:19 +0800

    feat(core): stylize object keys
    export camelizeFlags, stylizeFlags
    
    generated by ymc@robot

---
+++ packages/stylize-object-keys/readme.md
@@ -0,0 +1,18 @@
+# pkg stylize-object-keys
+
+## desc
+ymc package stylize-object-keys
+
+## lib size  
+file | size | gzip | brotli
+:---- | :---- | :---- | :----
+dist/index.js | 5.84kb | 1.71kb | 1.52kb
+dist/index.min.js | 2.20kb | 0.85kb | 0.75kb
+dist/index.esm.js | 5.31kb | 1.53kb | 1.36kb
+dist/index.esm.min.js | 1.91kb | 0.71kb | 0.64kb
+
+## Author
+yemiancheng <ymc.github@gmail.com>
+
+## License
+MIT
---
+++ packages/stylize-object-keys/src/index.js
@@ -0,0 +1,51 @@
+/* eslint-disable no-unused-vars,no-param-reassign */
+import * as styles from '@ymc/extend-string'
+// import  * as styles from '@ymc/extend-string/src/enhance'
+/**
+ * stylize param-json - nano-parser-flags
+ * @param {object} flags
+ * @param {likeCamelizeFlagsOption} options
+ * @returns
+ */
+function stylizeFlags(flags = {}, options = {}) {
+  // let res = {}
+  const option = {
+    slim: true,
+    style: 'camelize', // get more infomation on extend-string
+    ...options
+  }
+  if (option[`noAuto-${options.style}`.camelize()]) return flags
+  // typeof ''[options.style]==="function"
+  Object.keys(flags).forEach(str => {
+    // const ck = str[options.style]() //use when extend string.prototype
+    const ck = styles[options.style](str) // use when do not extend string.prototype
+    // res[ck]=flags[k]
+    if (ck !== str) {
+      flags[ck] = flags[str]
+      if (option.slim) {
+        delete flags[str]
+      }
+    }
+  })
+  return flags
+}
+
+/**
+ * camelize param-json - nano-parser-flags
+ * @param {object} flags
+ * @param {camelizeFlagsOption} options
+ * @returns
+ */
+function camelizeFlags(flags = {}, options = {}) {
+  // let res = {}
+  const option = {
+    slim: true,
+    ...options
+  }
+  option.style = 'camelize'
+  if (option.noAutoCamelize) return flags
+  stylizeFlags(flags, option)
+  return flags
+}
+
+export { camelizeFlags, stylizeFlags }

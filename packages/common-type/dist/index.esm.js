36b0b5f8694040455a3d16a408a7316f2b87e153
yemiancheng <ymc.github@gmail.com>
2022-10-28 21:33:05 +0800

    feat(core): add common type of jsdoc
    define common type in js file with jsdoc
    
    generated by ymc@robot

---
+++ packages/common-type/dist/index.esm.js
@@ -0,0 +1,63 @@
+/**
+  * commonType v1.0.0
+  * (c) 2018-2022 ymc
+  * @license MIT
+  */
+// Parameters:
+// Headers
+// Path parameters
+// Body  parameters
+// response status codes:
+
+/**
+ * @typedef {object} donwloadOption
+ * @property {string?} url - download from
+ * @property {string?} targetFile
+ * @property {boolean?} overideTargetFile
+ * @property {()=>{}} cutomRequest
+ */
+
+/**
+ * @typedef {object} showProgressOption
+ * @property {boolean?} showProgress
+ */
+
+/**
+ * @typedef {object} httpReqOption
+ * @property {object?} data
+ */
+
+/**
+ * @typedef {donwloadOption|showProgressOption|httpReqOption} donwloadFileOption
+ */
+
+/** @typedef {{url:string,file:string,name:string}} targetItem */
+/** @typedef {targetItem[]} targets */
+/** @typedef {string[]} urlmap */
+/** @typedef  {string} targetFile */
+
+// Maximum allowed is 100  max-len
+/** @typedef {{response:object,targetFile:string,stream?:object,resolve:Promise.resolve,data:string}} saveFileThroughStreamOption */ /* eslint-disable-line  max-len */
+/** @typedef {{file:string,cur:number,len:number,total:number}} showProgressNextOption */
+
+/** @typedef {[string,string][]} proxyDomainMap */
+
+/** @typedef {{prefix:string}} urlToDesOption */
+
+/** @typedef {{linkKeyAndVal:string,span:string}} pathParamTransferOption */
+
+/** @typedef {{noAutoCamelize?:boolean,slim?:boolean,mode?:string,modeStyle:string}} getBuiltinFlagsOption */ /* eslint-disable-line  max-len */
+/** @typedef {{noAutoCamelize?:boolean,slim?:boolean}} camelizeFlagsOption */
+/** @typedef {{noAutoCamelize?:boolean,slim?:boolean}} likeCamelizeFlagsOption */
+
+/** @typedef {{name:string,type:string,value:string|boolean,desc:string}} cliParam */
+/** @typedef {string} cliArgsStringExp */
+/** @typedef {string} httpQueryStringExp */
+/** @typedef {string} swithOptionStringExp */
+/** @typedef {object|cliArgsStringExp|httpQueryStringExp|swithOptionStringExp} getValFromParamResult */ /* eslint-disable-line  max-len */
+
+// [jsdoc:import typedef from another file](https://github.com/jsdoc/jsdoc/issues/1537)
+// [jsdoc:how to extend a typedef parameter](https://github.com/jsdoc/jsdoc/issues/1199)
+function noop() {}
+
+export { noop as default };
---
+++ packages/common-type/readme.md
@@ -0,0 +1,18 @@
+# pkg common-type
+
+## desc
+ymc package common-type of jsdoc
+
+## lib size  
+file | size | gzip | brotli
+:---- | :---- | :---- | :----
+dist/index.js | 2.48kb | 0.92kb | 0.77kb
+dist/index.min.js | 0.33kb | 0.23kb | 0.20kb
+dist/index.esm.js | 2.10kb | 0.78kb | 0.67kb
+dist/index.esm.min.js | 0.10kb | 0.11kb | 0.10kb
+
+## Author
+yemiancheng <ymc.github@gmail.com>
+
+## License
+MIT

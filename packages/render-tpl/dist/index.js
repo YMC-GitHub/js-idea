42428ba040748af719f13125ebf77b8ff97ff3cc
yemiancheng <ymc.github@gmail.com>
2022-10-29 16:25:24 +0800

    chore(core): update pack preset
    use cjs,esm,umd format
    use min version per format
    use esm without min as index.js
    
    generated by ymc@robot

--- packages/render-tpl/dist/index.js
+++ packages/render-tpl/dist/index.js
@@ -3,50 +3,36 @@
   * (c) 2018-2022 ymc
   * @license MIT
   */
-(function (global, factory) {
-  typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
-  typeof define === 'function' && define.amd ? define(['exports'], factory) :
-  (global = typeof globalThis !== 'undefined' ? globalThis : global || self, factory(global["render-tpl"] = {}));
-})(this, (function (exports) { 'use strict';
+/** @typedef {{[string]:string|boolean|number|undefined}} data */
+/**
+ * @param {string} tpl
+ * @param {data} data
+ * @returns {Stringimport("typescript").LiteralLike}
+ */
+function renderTpl(tpl, data) {
+  let res = tpl;
+  Object.keys(data).forEach(key => {
+    const value = data[key];
+    res = res.replace(new RegExp(`{${key}}`, 'ig'), value);
+  });
+  return res
+}
 
-  /** @typedef {{[string]:string|boolean|number|undefined}} data */
-
-  /**
-   * @param {string} tpl
-   * @param {data} data
-   * @returns {Stringimport("typescript").LiteralLike}
-   */
-  function renderTpl(tpl, data) {
-    let res = tpl;
-    Object.keys(data).forEach(key => {
-      const value = data[key];
-      res = res.replace(new RegExp(`{${key}}`, 'ig'), value);
-    });
-    return res;
+/**
+ *
+ * @param {string} tpl
+ * @param {data} data
+ * @returns {string|(data:data)=>string}
+ * @sample
+ * ```
+ * writeTpl('{method} repo/owner',{method:'POST'}) //POST repo/owner
+ * ```
+ */
+function writeTpl(tpl, data) {
+  if (data) {
+    return renderTpl(tpl, data)
   }
-  /**
-   *
-   * @param {string} tpl
-   * @param {data} data
-   * @returns {string|(data:data)=>string}
-   * @sample
-   * ```
-   * writeTpl('{method} repo/owner',{method:'POST'}) //POST repo/owner
-   * ```
-   */
+  return v => renderTpl(tpl, v)
+}
 
-
-  function writeTpl(tpl, data) {
-    if (data) {
-      return renderTpl(tpl, data);
-    }
-
-    return v => renderTpl(tpl, v);
-  }
-
-  exports.renderTpl = renderTpl;
-  exports.writeTpl = writeTpl;
-
-  Object.defineProperty(exports, '__esModule', { value: true });
-
-}));
+export { renderTpl, writeTpl };
--- packages/render-tpl/readme.md
+++ packages/render-tpl/readme.md
@@ -6,10 +6,13 @@ write or render tempalte - a small and simple template engine
 ## lib size  
 file | size | gzip | brotli
 :---- | :---- | :---- | :----
-dist/index.js | 1.27kb | 0.61kb | 0.51kb
-dist/index.min.js | 0.54kb | 0.36kb | 0.30kb
+dist/index.cjs.js | 0.80kb | 0.42kb | 0.36kb
+dist/index.cjs.min.js | 0.26kb | 0.21kb | 0.17kb
 dist/index.esm.js | 0.76kb | 0.41kb | 0.34kb
 dist/index.esm.min.js | 0.26kb | 0.22kb | 0.18kb
+dist/index.js | 0.76kb | 0.41kb | 0.34kb
+dist/index.umd.js | 1.27kb | 0.61kb | 0.51kb
+dist/index.umd.min.js | 0.54kb | 0.36kb | 0.30kb
 
 ## Author
 yemiancheng <ymc.github@gmail.com>

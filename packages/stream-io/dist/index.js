508757f69e83eab60ad42023912294ff406d1c43
yemiancheng <ymc.github@gmail.com>
2022-10-29 16:34:06 +0800

    chore(core): update pack preset
    use cjs,esm,umd format
    use min version per format
    use esm without min as index.js
    
    generated by ymc@robot

--- packages/stream-io/dist/index.js
+++ packages/stream-io/dist/index.js
@@ -3,42 +3,33 @@
   * (c) 2018-2022 ymc
   * @license MIT
   */
-(function (global, factory) {
-  typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
-  typeof define === 'function' && define.amd ? define(['exports'], factory) :
-  (global = typeof globalThis !== 'undefined' ? globalThis : global || self, factory(global["stream-io"] = {}));
-})(this, (function (exports) { 'use strict';
-
-  function readStream(stream) {
-    return new Promise((resolve, reject) => {
-      let data = '';
-      stream.on('data', chunk => {
+function readStream(stream) {
+  return new Promise((resolve, reject) => {
+    let data = '';
+    stream
+      .on('data', chunk => {
         data += chunk.toString();
-      }).on('end', () => {
+      })
+      .on('end', () => {
         resolve(data);
-      }).on('error', reject);
-    });
-  }
-
-  function writeStream({
-    stream,
-    data
-  }) {
-    return new Promise((resolve, reject) => {
-      // write
-      stream.write(data, 'utf-8'); // fire end
-
-      stream.end(); // desc-x-s: handle event finish and err
-
-      stream.on('finish', () => {
+      })
+      .on('error', reject);
+  })
+}
+function writeStream({ stream, data }) {
+  return new Promise((resolve, reject) => {
+    // write
+    stream.write(data, 'utf-8');
+    // fire end
+    stream.end();
+    // desc-x-s: handle event finish and err
+    stream
+      .on('finish', () => {
         resolve(data);
-      }).on('error', reject); // desc-x-e: handle event finish and err
-    });
-  }
+      })
+      .on('error', reject);
+    // desc-x-e: handle event finish and err
+  })
+}
 
-  exports.readStream = readStream;
-  exports.writeStream = writeStream;
-
-  Object.defineProperty(exports, '__esModule', { value: true });
-
-}));
+export { readStream, writeStream };
--- packages/stream-io/readme.md
+++ packages/stream-io/readme.md
@@ -6,10 +6,13 @@ async read or write data with stream
 ## lib size  
 file | size | gzip | brotli
 :---- | :---- | :---- | :----
-dist/index.js | 1.16kb | 0.51kb | 0.44kb
-dist/index.min.js | 0.63kb | 0.38kb | 0.32kb
+dist/index.cjs.js | 0.71kb | 0.32kb | 0.26kb
+dist/index.cjs.min.js | 0.35kb | 0.24kb | 0.19kb
 dist/index.esm.js | 0.72kb | 0.32kb | 0.27kb
 dist/index.esm.min.js | 0.36kb | 0.25kb | 0.21kb
+dist/index.js | 0.72kb | 0.32kb | 0.27kb
+dist/index.umd.js | 1.16kb | 0.51kb | 0.44kb
+dist/index.umd.min.js | 0.63kb | 0.38kb | 0.32kb
 
 ## Author
 yemiancheng <ymc.github@gmail.com>

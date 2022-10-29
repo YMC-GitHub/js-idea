d613ca84a5e43a6242b6702902f9598f10c33c7b
yemiancheng <ymc.github@gmail.com>
2022-10-29 16:17:45 +0800

    chore(core): update pack preset
    use cjs,esm,umd format
    use min version per format
    use esm without min as index.js
    
    generated by ymc@robot

--- packages/speed-up-github-by-url/dist/index.js
+++ packages/speed-up-github-by-url/dist/index.js
@@ -3,71 +3,56 @@
   * (c) 2018-2022 ymc
   * @license MIT
   */
-(function (global, factory) {
-  typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
-  typeof define === 'function' && define.amd ? define(['exports'], factory) :
-  (global = typeof globalThis !== 'undefined' ? globalThis : global || self, factory(global["speed-up-github-by-url"] = {}));
-})(this, (function (exports) { 'use strict';
+/* eslint-disable no-use-before-define */
+// fix infoProxyDomainMap' was used before it was defined
+const { log } = console;
 
-  /* eslint-disable no-use-before-define */
-  // fix infoProxyDomainMap' was used before it was defined
-  const {
-    log
-  } = console;
-  /** @typedef {[string,string][]} proxyDomainMap */
-  // pkg:
-  // @ymc/speed-up-github-by-url
+/** @typedef {[string,string][]} proxyDomainMap */
 
-  /**
-   * speedup github - add prefix url to matched url
-   * @param {string} url
-   * @param {{map?:proxyDomainMap,infoProxy:boolean}} option
-   * @returns {string}
-   */
-
-  function speedUpGithubByUrl(url, option = {}) {
-    let {
-      map
-    } = option;
-
-    if (!map) {
-      map = [['https://github.com', 'https://hub.fastgit.xyz'], ['https://raw.githubusercontent.com', 'https://raw.fastgit.org']];
-    }
-
-    if (option.infoProxy) {
-      log('[info] add prefix to matched url');
-      infoProxyDomainMap(map);
-    } // add prefix url when matched url
-
-
-    let res = url;
-
-    if (res) {
-      map.forEach(item => {
-        const [doamin, proxyByDomain] = item;
-        res = res.replace(new RegExp(`^${doamin}`, 'ig'), proxyByDomain);
-      });
-    }
-
-    return res;
+// pkg:
+// @ymc/speed-up-github-by-url
+/**
+ * speedup github - add prefix url to matched url
+ * @param {string} url
+ * @param {{map?:proxyDomainMap,infoProxy:boolean}} option
+ * @returns {string}
+ */
+function speedUpGithubByUrl(url, option = {}) {
+  let { map } = option;
+  if (!map) {
+    map = [
+      ['https://github.com', 'https://hub.fastgit.xyz'],
+      ['https://raw.githubusercontent.com', 'https://raw.fastgit.org']
+    ];
   }
-  /**
-   *
-   * @param {proxyDomainMap} map
-   */
+  if (option.infoProxy) {
+    log('[info] add prefix to matched url');
+    infoProxyDomainMap(map);
+  }
 
+  // add prefix url when matched url
+  let res = url;
+  if (res) {
+    map.forEach(item => {
+      const [doamin, proxyByDomain] = item;
+      res = res.replace(new RegExp(`^${doamin}`, 'ig'), proxyByDomain);
+    });
+  }
+  return res
+}
 
-  function infoProxyDomainMap(map) {
-    const res = map.map(v => {
+/**
+ *
+ * @param {proxyDomainMap} map
+ */
+function infoProxyDomainMap(map) {
+  const res = map
+    .map(v => {
       const [domain, proxyByDomain] = v;
-      return `proxy:${proxyByDomain} domain:${domain}`;
-    }).join('\n');
-    log(res);
-  }
+      return `proxy:${proxyByDomain} domain:${domain}`
+    })
+    .join('\n');
+  log(res);
+}
 
-  exports.infoProxy = infoProxyDomainMap;
-  exports.speedup = speedUpGithubByUrl;
-
-  Object.defineProperty(exports, '__esModule', { value: true });
-
-}));
+export { infoProxyDomainMap as infoProxy, speedUpGithubByUrl as speedup };
--- packages/speed-up-github-by-url/readme.md
+++ packages/speed-up-github-by-url/readme.md
@@ -6,10 +6,13 @@ speedup github - add prefix url to matched url
 ## lib size  
 file | size | gzip | brotli
 :---- | :---- | :---- | :----
-dist/index.js | 1.83kb | 0.82kb | 0.73kb
-dist/index.min.js | 0.81kb | 0.51kb | 0.45kb
+dist/index.cjs.js | 1.31kb | 0.63kb | 0.57kb
+dist/index.cjs.min.js | 0.51kb | 0.35kb | 0.30kb
 dist/index.esm.js | 1.32kb | 0.63kb | 0.57kb
 dist/index.esm.min.js | 0.52kb | 0.37kb | 0.32kb
+dist/index.js | 1.32kb | 0.63kb | 0.57kb
+dist/index.umd.js | 1.83kb | 0.82kb | 0.73kb
+dist/index.umd.min.js | 0.81kb | 0.51kb | 0.45kb
 
 ## Author
 yemiancheng <ymc.github@gmail.com>

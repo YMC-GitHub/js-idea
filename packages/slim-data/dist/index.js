c223359e5ee67d29d8fb6097839c7606e971466f
yemiancheng <ymc.github@gmail.com>
2022-10-29 16:21:14 +0800

    chore(core): update pack preset
    use cjs,esm,umd format
    use min version per format
    use esm without min as index.js
    
    generated by ymc@robot

--- packages/slim-data/dist/index.js
+++ packages/slim-data/dist/index.js
@@ -3,62 +3,56 @@
   * (c) 2018-2022 ymc
   * @license MIT
   */
-(function (global, factory) {
-  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
-  typeof define === 'function' && define.amd ? define(factory) :
-  (global = typeof globalThis !== 'undefined' ? globalThis : global || self, global["slim-data"] = factory());
-})(this, (function () { 'use strict';
+/* eslint-disable  no-use-before-define */
+// fix 'isString' was used before it was defined  no-use-before-define
+// Use array destructuring                    prefer-destructuring
 
-  /* eslint-disable  no-use-before-define */
-  // fix 'isString' was used before it was defined  no-use-before-define
-  // Use array destructuring                    prefer-destructuring
+/**
+ * slim data by keys
+ * @param {{}[]} data
+ * @param {{keys:string|string[],span?:string,excludeKeys:string|string[]}} option
+ * @returns {{}[]}
+ */
+function slimDataByKeys(data, option) {
+  const opts = {
+    span: ';',
+    excludeKeys: '',
+    ...option
+  };
 
-  /**
-   * slim data by keys
-   * @param {{}[]} data
-   * @param {{keys:string|string[],span?:string,excludeKeys:string|string[]}} option
-   * @returns {{}[]}
-   */
-  function slimDataByKeys(data, option) {
-    const opts = {
-      span: ';',
-      excludeKeys: '',
-      ...option
-    };
-    let list = opts.keys;
-    list = isString(list) ? list.split(opts.span) : list;
-    let excludeList = opts.excludeKeys;
-    excludeList = isString(excludeList) ? excludeList.split(opts.span) : excludeList;
-    list = list.filter(v => !excludeList.some(ex => ex === v));
-    let res;
-    let isOne;
-    let cache = Array.isArray(data) ? data : [data];
+  let list = opts.keys;
+  list = isString(list) ? list.split(opts.span) : list;
 
-    if (Array.isArray(data)) {
-      cache = data;
-    } else {
-      cache = [data];
-      isOne = true;
-    }
+  let excludeList = opts.excludeKeys;
+  excludeList = isString(excludeList) ? excludeList.split(opts.span) : excludeList;
+  list = list.filter(v => !excludeList.some(ex => ex === v));
 
-    res = cache.map(item => {
-      const slim = {};
-      list.forEach(key => {
-        if (key in item) {
-          slim[key] = item[key];
-        }
-      });
-      return slim;
-    }); // fix Use array destructuring                    prefer-destructuring
-
-    if (isOne) [res] = res;
-    return res;
+  let res;
+  let isOne;
+  let cache = Array.isArray(data) ? data : [data];
+  if (Array.isArray(data)) {
+    cache = data;
+  } else {
+    cache = [data];
+    isOne = true;
   }
 
-  function isString(s) {
-    return typeof s === 'string';
-  }
+  res = cache.map(item => {
+    const slim = {};
+    list.forEach(key => {
+      if (key in item) {
+        slim[key] = item[key];
+      }
+    });
+    return slim
+  });
 
-  return slimDataByKeys;
+  // fix Use array destructuring                    prefer-destructuring
+  if (isOne) [res] = res;
+  return res
+}
+function isString(s) {
+  return typeof s === 'string'
+}
 
-}));
+export { slimDataByKeys as default };
--- packages/slim-data/readme.md
+++ packages/slim-data/readme.md
@@ -6,10 +6,13 @@ slim data by keys
 ## lib size  
 file | size | gzip | brotli
 :---- | :---- | :---- | :----
-dist/index.js | 1.72kb | 0.72kb | 0.63kb
-dist/index.min.js | 0.67kb | 0.42kb | 0.37kb
+dist/index.cjs.js | 1.25kb | 0.54kb | 0.51kb
+dist/index.cjs.min.js | 0.39kb | 0.27kb | 0.26kb
 dist/index.esm.js | 1.31kb | 0.58kb | 0.53kb
 dist/index.esm.min.js | 0.45kb | 0.32kb | 0.31kb
+dist/index.js | 1.31kb | 0.58kb | 0.53kb
+dist/index.umd.js | 1.72kb | 0.72kb | 0.63kb
+dist/index.umd.min.js | 0.67kb | 0.42kb | 0.37kb
 
 ## Author
 yemiancheng <ymc.github@gmail.com>

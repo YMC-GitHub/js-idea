51e2b749747e7ece6b536221d2ae8720fab4b018
yemiancheng <ymc.github@gmail.com>
2022-10-29 21:45:19 +0800

    chore(core): update pack preset
    use cjs,esm,umd format
    use min version per format
    use esm without min as index.js
    
    generated by ymc@robot

--- packages/common-type/dist/index.js
+++ packages/common-type/dist/index.js
@@ -3,84 +3,61 @@
   * (c) 2018-2022 ymc
   * @license MIT
   */
-(function (global, factory) {
-	typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
-	typeof define === 'function' && define.amd ? define(factory) :
-	(global = typeof globalThis !== 'undefined' ? globalThis : global || self, global["common-type"] = factory());
-})(this, (function () { 'use strict';
+// Parameters:
+// Headers
+// Path parameters
+// Body  parameters
+// response status codes:
 
-	// Parameters:
-	// Headers
-	// Path parameters
-	// Body  parameters
-	// response status codes:
+/**
+ * @typedef {object} donwloadOption
+ * @property {string?} url - download from
+ * @property {string?} targetFile
+ * @property {boolean?} overideTargetFile
+ * @property {()=>{}} cutomRequest
+ */
 
-	/**
-	 * @typedef {object} donwloadOption
-	 * @property {string?} url - download from
-	 * @property {string?} targetFile
-	 * @property {boolean?} overideTargetFile
-	 * @property {()=>{}} cutomRequest
-	 */
+/**
+ * @typedef {object} showProgressOption
+ * @property {boolean?} showProgress
+ */
 
-	/**
-	 * @typedef {object} showProgressOption
-	 * @property {boolean?} showProgress
-	 */
+/**
+ * @typedef {object} httpReqOption
+ * @property {object?} data
+ */
 
-	/**
-	 * @typedef {object} httpReqOption
-	 * @property {object?} data
-	 */
+/**
+ * @typedef {donwloadOption|showProgressOption|httpReqOption} donwloadFileOption
+ */
 
-	/**
-	 * @typedef {donwloadOption|showProgressOption|httpReqOption} donwloadFileOption
-	 */
+/** @typedef {{url:string,file:string,name:string}} targetItem */
+/** @typedef {targetItem[]} targets */
+/** @typedef {string[]} urlmap */
+/** @typedef  {string} targetFile */
 
-	/** @typedef {{url:string,file:string,name:string}} targetItem */
+// Maximum allowed is 100  max-len
+/** @typedef {{response:object,targetFile:string,stream?:object,resolve:Promise.resolve,data:string}} saveFileThroughStreamOption */ /* eslint-disable-line  max-len */
+/** @typedef {{file:string,cur:number,len:number,total:number}} showProgressNextOption */
 
-	/** @typedef {targetItem[]} targets */
+/** @typedef {[string,string][]} proxyDomainMap */
 
-	/** @typedef {string[]} urlmap */
+/** @typedef {{prefix:string}} urlToDesOption */
 
-	/** @typedef  {string} targetFile */
-	// Maximum allowed is 100  max-len
+/** @typedef {{linkKeyAndVal:string,span:string}} pathParamTransferOption */
 
-	/** @typedef {{response:object,targetFile:string,stream?:object,resolve:Promise.resolve,data:string}} saveFileThroughStreamOption */
+/** @typedef {{noAutoCamelize?:boolean,slim?:boolean,mode?:string,modeStyle:string}} getBuiltinFlagsOption */ /* eslint-disable-line  max-len */
+/** @typedef {{noAutoCamelize?:boolean,slim?:boolean}} camelizeFlagsOption */
+/** @typedef {{noAutoCamelize?:boolean,slim?:boolean}} likeCamelizeFlagsOption */
 
-	/* eslint-disable-line  max-len */
+/** @typedef {{name:string,type:string,value:string|boolean,desc:string}} cliParam */
+/** @typedef {string} cliArgsStringExp */
+/** @typedef {string} httpQueryStringExp */
+/** @typedef {string} swithOptionStringExp */
+/** @typedef {object|cliArgsStringExp|httpQueryStringExp|swithOptionStringExp} getValFromParamResult */ /* eslint-disable-line  max-len */
 
-	/** @typedef {{file:string,cur:number,len:number,total:number}} showProgressNextOption */
+// [jsdoc:import typedef from another file](https://github.com/jsdoc/jsdoc/issues/1537)
+// [jsdoc:how to extend a typedef parameter](https://github.com/jsdoc/jsdoc/issues/1199)
+function noop() {}
 
-	/** @typedef {[string,string][]} proxyDomainMap */
-
-	/** @typedef {{prefix:string}} urlToDesOption */
-
-	/** @typedef {{linkKeyAndVal:string,span:string}} pathParamTransferOption */
-
-	/** @typedef {{noAutoCamelize?:boolean,slim?:boolean,mode?:string,modeStyle:string}} getBuiltinFlagsOption */
-
-	/* eslint-disable-line  max-len */
-
-	/** @typedef {{noAutoCamelize?:boolean,slim?:boolean}} camelizeFlagsOption */
-
-	/** @typedef {{noAutoCamelize?:boolean,slim?:boolean}} likeCamelizeFlagsOption */
-
-	/** @typedef {{name:string,type:string,value:string|boolean,desc:string}} cliParam */
-
-	/** @typedef {string} cliArgsStringExp */
-
-	/** @typedef {string} httpQueryStringExp */
-
-	/** @typedef {string} swithOptionStringExp */
-
-	/** @typedef {object|cliArgsStringExp|httpQueryStringExp|swithOptionStringExp} getValFromParamResult */
-
-	/* eslint-disable-line  max-len */
-	// [jsdoc:import typedef from another file](https://github.com/jsdoc/jsdoc/issues/1537)
-	// [jsdoc:how to extend a typedef parameter](https://github.com/jsdoc/jsdoc/issues/1199)
-	function noop() {}
-
-	return noop;
-
-}));
+export { noop as default };
--- packages/common-type/readme.md
+++ packages/common-type/readme.md
@@ -6,10 +6,13 @@ ymc package common-type of jsdoc
 ## lib size  
 file | size | gzip | brotli
 :---- | :---- | :---- | :----
-dist/index.js | 2.48kb | 0.92kb | 0.77kb
-dist/index.min.js | 0.33kb | 0.23kb | 0.20kb
+dist/index.cjs.js | 2.12kb | 0.78kb | 0.67kb
+dist/index.cjs.min.js | 0.11kb | 0.12kb | 0.08kb
 dist/index.esm.js | 2.10kb | 0.78kb | 0.67kb
 dist/index.esm.min.js | 0.10kb | 0.11kb | 0.10kb
+dist/index.js | 2.10kb | 0.78kb | 0.67kb
+dist/index.umd.js | 2.48kb | 0.92kb | 0.77kb
+dist/index.umd.min.js | 0.33kb | 0.23kb | 0.20kb
 
 ## Author
 yemiancheng <ymc.github@gmail.com>

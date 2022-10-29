33f2304d8a64fb6ad1eee118dfd2426cdd92e25d
yemiancheng <ymc.github@gmail.com>
2022-10-29 21:45:15 +0800

    chore(core): update pack preset
    use cjs,esm,umd format
    use min version per format
    use esm without min as index.js
    
    generated by ymc@robot

--- packages/cli-flags/dist/index.js
+++ packages/cli-flags/dist/index.js
@@ -3,143 +3,125 @@
   * (c) 2018-2022 ymc
   * @license MIT
   */
-(function (global, factory) {
-  typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
-  typeof define === 'function' && define.amd ? define(['exports'], factory) :
-  (global = typeof globalThis !== 'undefined' ? globalThis : global || self, factory(global["cli-flags"] = {}));
-})(this, (function (exports) { 'use strict';
+/**
+  * extendString v1.0.0
+  * (c) 2018-2022 ymc
+  * @license MIT
+  */
 
-  /**
-    * extendString v1.0.0
-    * (c) 2018-2022 ymc
-    * @license MIT
-    */
-  /**
-   *
-   * @param {*} s
-   * @returns {string}
-   * @sample
-   * ```
-   * humanize('per_page')// Per page
-   * humanize('per-page')// Per page
-   * ```
-   * @description
-   * ```
-   * ## idea
-   * - [x] replace multi - or _ to one space
-   * - [x] add space to the char that is uppercase and is not the first index
-   * - [x] the first char to upper ,other lowercase
-   * ```
-   */
-
-
-  function humanize(s) {
-    return s.replace(/(?:^\w|[A-Z_-]|\b\w)/g, (word, index) => {
-      let res = ''; // log(word, index); //desc: for debug
+/**
+ *
+ * @param {*} s
+ * @returns {string}
+ * @sample
+ * ```
+ * humanize('per_page')// Per page
+ * humanize('per-page')// Per page
+ * ```
+ * @description
+ * ```
+ * ## idea
+ * - [x] replace multi - or _ to one space
+ * - [x] add space to the char that is uppercase and is not the first index
+ * - [x] the first char to upper ,other lowercase
+ * ```
+ */
+function humanize(s) {
+  return s
+    .replace(/(?:^\w|[A-Z_-]|\b\w)/g, (word, index) => {
+      let res = '';
+      // log(word, index); //desc: for debug
       // feat: replace multi - or _ to one space
+      res = word.replace(/[-_]+/g, ' ');
+      // feat: add space to the char that is uppercase and is not the first index
+      res = index !== 0 ? res.replace(/[A-Z]/, ' $&') : res;
+      // feat: the first char to upper ,other lowercase
+      return index === 0 ? res.toUpperCase() : res.toLowerCase()
+    })
+    .replace(/\s+/g, ' ')
+}
 
-      res = word.replace(/[-_]+/g, ' '); // feat: add space to the char that is uppercase and is not the first index
+function camelize(s) {
+  return humanize(s)
+    .replace(/(?:^\w|[A-Z]|\b\w)/g, (word, index) => (index === 0 ? word.toLowerCase() : word.toUpperCase()))
+    .replace(/\s+/g, '')
+}
 
-      res = index !== 0 ? res.replace(/[A-Z]/, ' $&') : res; // feat: the first char to upper ,other lowercase
+function underscoped(s) {
+  return humanize(s)
+    .replace(/(?:^\w|[A-Z]|\b\w)/g, (word, index) => word.toLowerCase())
+    .replace(/\s+/g, '_')
+}
 
-      return index === 0 ? res.toUpperCase() : res.toLowerCase();
-    }).replace(/\s+/g, ' ');
-  }
+/* eslint-disable no-param-reassign */
 
-  function camelize(s) {
-    return humanize(s).replace(/(?:^\w|[A-Z]|\b\w)/g, (word, index) => index === 0 ? word.toLowerCase() : word.toUpperCase()).replace(/\s+/g, '');
-  }
+/** @typedef {{noAutoCamelize?:boolean,slim?:boolean}} camelizeFlagsOption */
+/** @typedef {{noAutoCamelize?:boolean,slim?:boolean}} likeCamelizeFlagsOption */
 
-  function underscoped(s) {
-    return humanize(s).replace(/(?:^\w|[A-Z]|\b\w)/g, (word, index) => word.toLowerCase()).replace(/\s+/g, '_');
-  }
+/** @typedef {{style:string,styleHandle:()=>string}} stylizeOption */
 
-  /* eslint-disable no-param-reassign */
-  /** @typedef {{noAutoCamelize?:boolean,slim?:boolean}} camelizeFlagsOption */
-
-  /** @typedef {{noAutoCamelize?:boolean,slim?:boolean}} likeCamelizeFlagsOption */
-
-  /** @typedef {{style:string,styleHandle:()=>string}} stylizeOption */
-
-  /**
-   * stylize param-json - nano-parser-flags
-   * @param {object} flags
-   * @param {likeCamelizeFlagsOption & stylizeOption} options
-   * @returns
-   */
-
-  function stylizeFlags(flags = {}, options = {}) {
-    // let res = {}
-    const option = {
-      slim: true,
-      style: 'camelize',
-      // get more infomation on extend-string
-      styleHandle: camelize,
-      ...options
-    };
-    const stylize = option.styleHandle;
-    if (option[camelize(`noAuto-${options.style}`)]) return flags; // typeof ''[options.style]==="function"
-
-    Object.keys(flags).forEach(str => {
-      const ck = stylize(str); // res[ck]=flags[k]
-
-      if (ck !== str) {
-        flags[ck] = flags[str];
-
-        if (option.slim) {
-          delete flags[str];
-        }
+/**
+ * stylize param-json - nano-parser-flags
+ * @param {object} flags
+ * @param {likeCamelizeFlagsOption & stylizeOption} options
+ * @returns
+ */
+function stylizeFlags(flags = {}, options = {}) {
+  // let res = {}
+  const option = {
+    slim: true,
+    style: 'camelize', // get more infomation on extend-string
+    styleHandle: camelize,
+    ...options
+  };
+  const stylize = option.styleHandle;
+  if (option[camelize(`noAuto-${options.style}`)]) return flags
+  // typeof ''[options.style]==="function"
+  Object.keys(flags).forEach(str => {
+    const ck = stylize(str);
+    // res[ck]=flags[k]
+    if (ck !== str) {
+      flags[ck] = flags[str];
+      if (option.slim) {
+        delete flags[str];
       }
-    });
-    return flags;
-  }
-  /**
-   * camelize param-json - nano-parser-flags
-   * @param {object} flags
-   * @param {camelizeFlagsOption} options
-   * @returns
-   */
-
-
-  function camelizeFlags(flags = {}, options = {}) {
-    // let res = {}
-    const option = {
-      slim: true,
-      ...options
-    };
-    if (option.noAutoCamelize) return flags;
-    Object.keys(flags).forEach(k => {
-      const ck = camelize(k); // res[ck]=flags[k]
-
-      if (ck !== k) {
-        flags[ck] = flags[k];
-
-        if (option.slim) {
-          delete flags[k];
-        }
+    }
+  });
+  return flags
+}
+/**
+ * camelize param-json - nano-parser-flags
+ * @param {object} flags
+ * @param {camelizeFlagsOption} options
+ * @returns
+ */
+function camelizeFlags(flags = {}, options = {}) {
+  // let res = {}
+  const option = {
+    slim: true,
+    ...options
+  };
+  if (option.noAutoCamelize) return flags
+  Object.keys(flags).forEach(k => {
+    const ck = camelize(k);
+    // res[ck]=flags[k]
+    if (ck !== k) {
+      flags[ck] = flags[k];
+      if (option.slim) {
+        delete flags[k];
       }
-    });
-    return flags;
-  }
-  /**
-   * underscoped param-json - nano-parser-flags
-   * @param {object} flags
-   * @param {likeCamelizeFlagsOption} options
-   * @returns
-   */
+    }
+  });
+  return flags
+}
+/**
+ * underscoped param-json - nano-parser-flags
+ * @param {object} flags
+ * @param {likeCamelizeFlagsOption} options
+ * @returns
+ */
+function underscopedFlags(flags = {}, options = {}) {
+  return stylizeFlags(flags, { ...options, style: 'underscoped', styleHandle: underscoped })
+}
 
-
-  function underscopedFlags(flags = {}, options = {}) {
-    return stylizeFlags(flags, { ...options,
-      style: 'underscoped',
-      styleHandle: underscoped
-    });
-  }
-
-  exports.camelizeFlags = camelizeFlags;
-  exports.stylizeFlags = stylizeFlags;
-  exports.underscopedFlags = underscopedFlags;
-
-  Object.defineProperty(exports, '__esModule', { value: true });
-
-}));
+export { camelizeFlags, stylizeFlags, underscopedFlags };
--- packages/cli-flags/readme.md
+++ packages/cli-flags/readme.md
@@ -6,10 +6,13 @@ stylize flags ,camelize  underscoped and so on
 ## lib size  
 file | size | gzip | brotli
 :---- | :---- | :---- | :----
-dist/index.js | 3.82kb | 1.20kb | 1.05kb
-dist/index.min.js | 1.33kb | 0.61kb | 0.54kb
-dist/index.esm.js | 3.24kb | 1.00kb | 0.90kb
+dist/index.cjs.js | 3.27kb | 1.05kb | 0.92kb
+dist/index.cjs.min.js | 1.12kb | 0.52kb | 0.45kb
+dist/index.esm.js | 3.16kb | 0.99kb | 0.89kb
 dist/index.esm.min.js | 1.05kb | 0.49kb | 0.43kb
+dist/index.js | 3.16kb | 0.99kb | 0.89kb
+dist/index.umd.js | 3.82kb | 1.20kb | 1.05kb
+dist/index.umd.min.js | 1.33kb | 0.61kb | 0.54kb
 
 ## Author
 yemiancheng <ymc.github@gmail.com>

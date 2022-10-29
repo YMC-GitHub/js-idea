a323c43843b6c804eb0c02492d2c79fc79ff1018
yemiancheng <ymc.github@gmail.com>
2022-10-29 16:36:55 +0800

    feat(core): async read or write data with stream
    export JsonStream
    export jsonStream
    
    generated by ymc@robot

--- packages/json-stream-io/dist/index.js
+++ packages/json-stream-io/dist/index.js
@@ -3,143 +3,118 @@
   * (c) 2018-2022 ymc
   * @license MIT
   */
-(function (global, factory) {
-  typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports, require('node:fs')) :
-  typeof define === 'function' && define.amd ? define(['exports', 'node:fs'], factory) :
-  (global = typeof globalThis !== 'undefined' ? globalThis : global || self, factory(global["json-stream-io"] = {}, global.node_fs));
-})(this, (function (exports, node_fs) { 'use strict';
+import { createReadStream, createWriteStream } from 'node:fs';
 
-  /**
-    * streamIo v1.0.0
-    * (c) 2018-2022 ymc
-    * @license MIT
-    */
-  function readStream(stream) {
-    return new Promise((resolve, reject) => {
-      let data = '';
-      stream.on('data', chunk => {
+/**
+  * streamIo v1.0.0
+  * (c) 2018-2022 ymc
+  * @license MIT
+  */
+function readStream(stream) {
+  return new Promise((resolve, reject) => {
+    let data = '';
+    stream
+      .on('data', chunk => {
         data += chunk.toString();
-      }).on('end', () => {
+      })
+      .on('end', () => {
         resolve(data);
-      }).on('error', reject);
-    });
-  }
-
-  function writeStream({
-    stream,
-    data
-  }) {
-    return new Promise((resolve, reject) => {
-      // write
-      stream.write(data, 'utf-8'); // fire end
-
-      stream.end(); // desc-x-s: handle event finish and err
-
-      stream.on('finish', () => {
+      })
+      .on('error', reject);
+  })
+}
+function writeStream({ stream, data }) {
+  return new Promise((resolve, reject) => {
+    // write
+    stream.write(data, 'utf-8');
+    // fire end
+    stream.end();
+    // desc-x-s: handle event finish and err
+    stream
+      .on('finish', () => {
         resolve(data);
-      }).on('error', reject); // desc-x-e: handle event finish and err
-    });
+      })
+      .on('error', reject);
+    // desc-x-e: handle event finish and err
+  })
+}
+
+/**
+ * @sample
+ * ```
+ * jsonstream.file.name="package.json"
+ * //or
+ * jsonstream.init("package.json")
+ * await jsonstream.read()
+ * await jsonstream.write({})
+ * ```
+ */
+class JsonStream {
+  constructor(name, data) {
+    this.init(name, data);
   }
 
   /**
-   * @sample
-   * ```
-   * jsonstream.file.name="package.json"
-   * //or
-   * jsonstream.init("package.json")
-   * await jsonstream.read()
-   * await jsonstream.write({})
-   * ```
+   * read file async (stream mode)
+   * @param {{}|[]} def
+   * @returns {Prmosie<json>}
    */
-
-  class JsonStream {
-    constructor(name, data) {
-      this.init(name, data);
+  async read(def = {}) {
+    const { file } = this;
+    let reader;
+    let res;
+    try {
+      reader = createReadStream(file.name);
+      res = await readStream(reader);
+      res = JSON.parse(res);
+    } catch (error) {
+      // console.log(error);
+      res = def;
     }
-    /**
-     * read file async (stream mode)
-     * @param {{}|[]} def
-     * @returns {Prmosie<json>}
-     */
-
-
-    async read(def = {}) {
-      const {
-        file
-      } = this;
-      let reader;
-      let res;
-
-      try {
-        reader = node_fs.createReadStream(file.name);
-        res = await readStream(reader);
-        res = JSON.parse(res);
-      } catch (error) {
-        // console.log(error);
-        res = def;
-      }
-
-      file.data = res;
-      return res;
-    }
-    /**
-     * write file async (stream mode)
-     * @param {{}|[]|undefined} data
-     * @returns {Prmosie<void>}
-     */
-
-
-    async write(data) {
-      // no-param-reassign data
-      // no-unused-vars option
-
-      /* eslint-disable no-unused-vars */
-      const {
-        file,
-        option
-      } = this; // eslint-disable-line
-
-      let writer;
-      let content = data;
-
-      try {
-        writer = node_fs.createWriteStream(file.name);
-
-        if (data) {
-          file.data = data;
-        } else {
-          content = file.data;
-        }
-
-        await writeStream({
-          stream: writer,
-          data: JSON.stringify(content, null, 2)
-        });
-      } catch (error) {
-      }
-    }
-
-    init(name = 'package.json', data = {}) {
-      this.file = {
-        name,
-        data
-      };
-      this.option = {};
-    }
-    /* eslint-disable class-methods-use-this */
-
-
-    new(...option) {
-      return new JsonStream(...option);
-    }
-
+    file.data = res;
+    return res
   }
 
-  const jsonstream = new JsonStream();
+  /**
+   * write file async (stream mode)
+   * @param {{}|[]|undefined} data
+   * @returns {Prmosie<void>}
+   */
+  async write(data) {
+    // no-param-reassign data
+    // no-unused-vars option
+    /* eslint-disable no-unused-vars */
+    const { file, option } = this; // eslint-disable-line
+    let writer;
+    let content = data;
+    try {
+      writer = createWriteStream(file.name);
+      if (data) {
+        file.data = data;
+      } else {
+        content = file.data;
+      }
+      await writeStream({
+        stream: writer,
+        data: JSON.stringify(content, null, 2)
+      });
+    } catch (error) {
+    }
+  }
 
-  exports.JsonStream = JsonStream;
-  exports.jsonstream = jsonstream;
+  init(name = 'package.json', data = {}) {
+    this.file = {
+      name,
+      data
+    };
+    this.option = {};
+  }
 
-  Object.defineProperty(exports, '__esModule', { value: true });
+  /* eslint-disable class-methods-use-this */
+  new(...option) {
+    return new JsonStream(...option)
+  }
+}
+const jsonstream = new JsonStream();
 
-}));
+export { JsonStream, jsonstream };
--- packages/json-stream-io/readme.md
+++ packages/json-stream-io/readme.md
@@ -6,10 +6,13 @@ async read or write json file with stream
 ## lib size  
 file | size | gzip | brotli
 :---- | :---- | :---- | :----
-dist/index.js | 3.03kb | 1.10kb | 0.95kb
-dist/index.min.js | 1.19kb | 0.64kb | 0.56kb
+dist/index.cjs.js | 2.38kb | 0.93kb | 0.81kb
+dist/index.cjs.min.js | 0.88kb | 0.52kb | 0.44kb
 dist/index.esm.js | 2.37kb | 0.87kb | 0.75kb
 dist/index.esm.min.js | 0.89kb | 0.48kb | 0.44kb
+dist/index.js | 2.37kb | 0.87kb | 0.75kb
+dist/index.umd.js | 3.03kb | 1.10kb | 0.95kb
+dist/index.umd.min.js | 1.19kb | 0.64kb | 0.56kb
 
 ## Author
 yemiancheng <ymc.github@gmail.com>

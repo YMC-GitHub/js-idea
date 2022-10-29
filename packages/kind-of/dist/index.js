36d268b34169b1fa0c73b51fd6e126a59cdb620e
yemiancheng <ymc.github@gmail.com>
2022-10-29 21:45:22 +0800

    chore(core): update pack preset
    use cjs,esm,umd format
    use min version per format
    use esm without min as index.js
    
    generated by ymc@robot

--- packages/kind-of/dist/index.js
+++ packages/kind-of/dist/index.js
@@ -3,64 +3,46 @@
   * (c) 2018-2022 ymc
   * @license MIT
   */
-(function (global, factory) {
-  typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
-  typeof define === 'function' && define.amd ? define(['exports'], factory) :
-  (global = typeof globalThis !== 'undefined' ? globalThis : global || self, factory(global["kind-of"] = {}));
-})(this, (function (exports) { 'use strict';
+// @ymc/kind-of # @ymc/type-of
+// @ymc/is-type # @ymc/is
+const { toString } = Object.prototype;
 
-  // @ymc/kind-of # @ymc/type-of
-  // @ymc/is-type # @ymc/is
-  const {
-    toString
-  } = Object.prototype;
+/**
+ * get kind of thing
+ * @param {*} thing
+ * @returns {string}
+ * @description
+ * ```
+ * ## good ?
+ * - [x] cache result
+ * - [x] zero middle var with iifn
+ * ```
+ */
+const kindOf = (cache => thing => {
+  const str = toString.call(thing);
+  return cache[str] || (cache[str] = str.slice(8, -1).toLowerCase()) //eslint-disable-line
+  // Return statement should not contain assignment        no-return-assign
+  // Assignment to property of function parameter 'cache' no-param-reassign
+})(Object.create(null));
 
-  /**
-   * get kind of thing
-   * @param {*} thing
-   * @returns {string}
-   * @description
-   * ```
-   * ## good ?
-   * - [x] cache result
-   * - [x] zero middle var with iifn
-   * ```
-   */
+/**
+ *
+ * @param {string} type
+ * @description
+ * ```
+ * idea:to-lowercase -> kind-of -> eq-type
+ * ```
+ */
+const kindOfTest = type => thing => kindOf(thing) === type.toLowerCase();
 
-  const kindOf = (cache => thing => {
-    const str = toString.call(thing);
-    return cache[str] || (cache[str] = str.slice(8, -1).toLowerCase()); //eslint-disable-line
-    // Return statement should not contain assignment        no-return-assign
-    // Assignment to property of function parameter 'cache' no-param-reassign
-  })(Object.create(null));
-  /**
-   *
-   * @param {string} type
-   * @description
-   * ```
-   * idea:to-lowercase -> kind-of -> eq-type
-   * ```
-   */
+/**
+ *
+ * @param {*} type
+ * @description
+ * ```
+ * idea:type-of -> eq-type
+ * ```
+ */
+const typeOfTest = type => thing => typeof thing === type; //eslint-disable-line
 
-
-  const kindOfTest = type => thing => kindOf(thing) === type.toLowerCase();
-  /**
-   *
-   * @param {*} type
-   * @description
-   * ```
-   * idea:type-of -> eq-type
-   * ```
-   */
-
-
-  const typeOfTest = type => thing => typeof thing === type; //eslint-disable-line
-
-  exports.kindOf = kindOf;
-  exports.kindOfTest = kindOfTest;
-  exports.toString = toString;
-  exports.typeOfTest = typeOfTest;
-
-  Object.defineProperty(exports, '__esModule', { value: true });
-
-}));
+export { kindOf, kindOfTest, toString, typeOfTest };
--- packages/kind-of/readme.md
+++ packages/kind-of/readme.md
@@ -6,10 +6,13 @@ get type of val - enhance typeof
 ## lib size  
 file | size | gzip | brotli
 :---- | :---- | :---- | :----
-dist/index.js | 1.62kb | 0.70kb | 0.61kb
-dist/index.min.js | 0.58kb | 0.37kb | 0.34kb
+dist/index.cjs.js | 1.20kb | 0.56kb | 0.46kb
+dist/index.cjs.min.js | 0.39kb | 0.28kb | 0.23kb
 dist/index.esm.js | 1.05kb | 0.50kb | 0.41kb
 dist/index.esm.min.js | 0.32kb | 0.24kb | 0.19kb
+dist/index.js | 1.05kb | 0.50kb | 0.41kb
+dist/index.umd.js | 1.62kb | 0.70kb | 0.61kb
+dist/index.umd.min.js | 0.58kb | 0.37kb | 0.34kb
 
 ## Author
 yemiancheng <ymc.github@gmail.com>

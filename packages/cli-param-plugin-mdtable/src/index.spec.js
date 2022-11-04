97b5f7c6cc3730d8a9234a96b7dcc65d2d81d99e
yemiancheng <ymc.github@gmail.com>
2022-11-04 23:23:11 +0800

    style(core): lin code
    to keep zero error,warn
    to keep package.json to be not-modified
    
    generated by ymc@robot

--- packages/cli-param-plugin-mdtable/src/index.spec.js
+++ packages/cli-param-plugin-mdtable/src/index.spec.js
@@ -1,18 +1,18 @@
 import main from './index'
 test(`param-to-markdown-table`, () => {
-    let param = [{ name: '-h,--help', type: 'boolean', value: false, desc: 'info help' }]
-    let output
-    output = main(param)
-    let exp = `## param
+  let param = [{ name: '-h,--help', type: 'boolean', value: false, desc: 'info help' }]
+  let output
+  output = main(param)
+  let exp = `## param
 name|type|value|desc|optional
 :--|:--|:--|:--|:--
 help|boolean|false|info help|\n\n`
-    expect(output).toStrictEqual(exp)
+  expect(output).toStrictEqual(exp)
 
-    output = main(param, '## param', { slimName: false, camelizeName: false })
-    exp = `## param
+  output = main(param, '## param', { slimName: false, camelizeName: false })
+  exp = `## param
 name|type|value|desc|optional
 :--|:--|:--|:--|:--
 -h,--help|boolean|false|info help|\n\n`
-    expect(output).toStrictEqual(exp)
+  expect(output).toStrictEqual(exp)
 })

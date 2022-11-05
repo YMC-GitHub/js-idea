295b3b4ad53a8d6b99d123285a3681dd55babb94
yemiancheng <ymc.github@gmail.com>
2022-11-05 14:30:36 +0800

    test(core): add base help
    test mock function
    basename,dirname
    
    generated by ymc@robot

---
+++ packages/pkgs-info/src/mock.spec.js
@@ -0,0 +1,9 @@
+import { basename, dirname } from './mock'
+let input = `packages/noop`
+
+test(`mock-path-dirname`, () => {
+  expect(dirname(input)).toStrictEqual(`packages`)
+})
+test(`mock-path-basename`, () => {
+  expect(basename(input)).toStrictEqual(`noop`)
+})

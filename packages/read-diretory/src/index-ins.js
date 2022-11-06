4eda8b4ac5f05472ffbd031685b15861c817f1ca
yemiancheng <ymc.github@gmail.com>
2022-11-06 23:15:32 +0800

    chore(core): update demo
    add more case
    allow cli input
    
    generated by ymc@robot

--- packages/read-diretory/src/index-ins.js
+++ packages/read-diretory/src/index-ins.js
@@ -1,37 +1,170 @@
-// fix Unexpected use of file extension "js" for "./index.js"      import/extensions
-import getDstDir from './index'
-import { log, blue } from './index-too'
+/* eslint-disable max-len */
+/* eslint-disable no-unused-vars */
+/* eslint-disable prefer-const */
+/* eslint-disable no-new-func */
+/* eslint-disable no-shadow */
 // sam here
+import { getBuiltinConfig, getCliFlags } from '@ymc/cli-param'
+import paeseArgs from '@ymc/nano-parse'
+import { baseParam } from '@ymc/cli-preset-param'
+import { log, blue } from './index-too'
+import getDstDir from './index'
 
 const option = {
   excludes: ['dist', 'lib', 'libtpl-rollup-plugins', 'vagrant']
 }
-let result = []
-// docs: update usage comment in instance\nwith xx to option.xx
-// log(blue(`get dst dir: node_modules`))
-// getDstDir("D:\\code-store",/node_modules/ig)
+function param() {
+  return [
+    ...baseParam(),
+    {
+      name: '--find-in-dir',
+      type: 'string',
+      value: './',
+      desc: 'the location to find'
+    },
+    {
+      name: '--mode',
+      type: 'string',
+      value: '',
+      desc: 'the mode how to find, file | file_text'
+    },
+    {
+      name: '--regexp',
+      type: 'regexp',
+      value: '',
+      desc: 'the regexp to match file path'
+    },
+    {
+      name: '--desc',
+      type: 'string',
+      value: '',
+      desc: 'the desc for the task'
+    },
+    {
+      name: '--file-text-regexp',
+      type: 'regexp',
+      value: '',
+      desc: 'the regexp to match file text'
+    },
+    {
+      name: '--exclude',
+      type: 'string',
+      value: 'dist|lib|libtpl-rollup-plugins|vagrant',
+      desc: 'the loc of no finding'
+    }
+  ]
+}
+function runsam(cnf) {
+  // log(blue(cnf.desc))
+  log(`[task] ${cnf.desc}`)
+  // let opt = {
+  //     ...option,
+  //     ...cnf
+  // }
+  // option.mode = cnf.mode
+  // option.regexp = cnf.regexp
+  const res = getDstDir(cnf.loc, cnf)
+  log(res)
+}
+function conf(opts) {
+  return { ...option, ...opts }
+}
+function mainold() {
+  const sams = {}
+  // find file path
+  sams['get-hzh-sh'] = conf({
+    loc: 'D:\\code-store',
+    mode: 'file',
+    regexp: /hzh.*\.sh$/gi,
+    desc: 'get dst file: the file that file text with hzh and name ends with .sh'
+  })
 
-// log(blue(`get dst dir: the dirs that with empty space`))
-// getDstDir("F:\\bilibili-dance",/ +/ig)
+  sams['get-fav-js'] = conf({
+    loc: 'D:\\code-store',
+    mode: 'file',
+    regexp: /fav.*\.js$/gi,
+    desc: 'get dst file: the file that name with fav and ends with .js'
+  })
+  sams['get-copy-js'] = conf({
+    loc: 'D:\\code-store',
+    mode: 'file',
+    regexp: /copy.*\.js$/gi,
+    desc: 'get dst file: the file that name with copy and ends with .js'
+  })
 
-// log(blue(`get dst file: the file that name with copy and ends with .js`))
-// option.mode='file'
-// getDstDir("D:\\code-store",/copy.*\.js$/ig)
+  // find dir
+  sams['get-space-dir'] = conf({
+    loc: 'F:\\bilibili-dance',
+    mode: '',
+    regexp: / +/gi,
+    desc: 'get dst dir: the dirs that with empty space'
+  })
+  sams['get-nodemodule-dir'] = conf({
+    loc: 'D:\\code-store',
+    mode: '',
+    regexp: /node_modules/gi,
+    desc: 'get dst dir: node_modules'
+  })
 
-// log(blue(`get dst file: the file that name with fav and ends with .js`))
-// option.mode='file'
-// getDstDir("D:\\code-store",/fav.*\.js$/ig)
+  sams['get-ycs-file'] = conf({
+    loc: 'H:\\js-project',
+    mode: 'file_text',
+    fileTextRegexp: /ycs/gi,
+    desc: 'get dst file: the file that file text with ycs and name ends with .js'
+  })
 
-log(blue('get dst file: the file that name with hua and ends with .sh'))
-// option.mode = 'file'
-option.regexp = /hzh.*\.sh$/gi
-result = getDstDir('D:\\code-store', option)
-log(result)
-// log(blue(`get dst file: the file that file text with ycs and name ends with .js`))
-// option.mode='file_text'
-// option.fileTextRegexp=/ycs/ig
-// getDstDir("H:\\js-project",/\.js$/ig)
+  let cnf
+  cnf = sams['get-hzh-sh']
+  runsam(cnf)
+}
+function getRegFromCli(s) {
+  // s = '/ycs/gi'
+  let res = s
+  if (res) {
+    res = res.replace(/^\\/, '') // escape regexp the first / in cli
+    return new Function(`return ${res}`)()
+  }
+  return ''
+}
+function makeDescFromOpt(options = {}) {
+  let res
+  if (options.mode === '') {
+    res = 'get dst dir:'
+    res = `${res} with file-path reg ${options.regexp} in loc ${options.findInDir}`
+  } else if (options.mode === 'file') {
+    res = 'get dst file:'
+    res = `${res} with file-path reg ${options.regexp} in loc ${options.findInDir}`
+  } else {
+    res = 'get dst file:'
+    res = `${res} with file-path reg ${options.regexp} file-text reg ${options.fileTextRegexp} in loc ${options.findInDir}`
+  }
+  return res
+}
+function main(options = {}) {
+  const option = {
+    ...getBuiltinConfig(param()),
+    ...getCliFlags(options)
+  }
+  // log(option)
+  // set exclude to array
+  option.excludes = option.exclude.split(/ |\||,/gi)
 
+  // set cli-str to js-regexp from cli
+  option.regexp = getRegFromCli(option.regexp)
+  option.fileTextRegexp = getRegFromCli(option.fileTextRegexp)
+  option.excludesRegexp = getRegFromCli(option.excludesRegexp)
+
+  // set desc
+  // desc: `get dst file: the file that name with copy and ends with .js`
+  option.desc = makeDescFromOpt(option)
+  option.loc = option.findInDir
+
+  log(option)
+  runsam(option)
+}
+
+main(paeseArgs(process.argv))
+// log(getRegFromCli())
 // log(blue(`get dst file: the file that file text with ycs and name ends with .js`))
 // option.mode='file_text'
 // option.fileTextRegexp=/curd-dirs-sync/ig
@@ -49,4 +182,8 @@ log(result)
 // option.fileTextRegexp=/eslint/ig
 // option.excludesRegexp=/node_modules/ig
 // getDstDir("D:\\code-store\\typescript",/\.js$/ig)
-// node packages/read-diretory/src/index-ins.js
+// node --no-warnings --loader ./scr/lib/esm-loader.js packages/read-diretory/src/index-ins.js
+// task:get-lod-code:txt-to-audio
+// node --no-warnings --loader ./scr/lib/esm-loader.js packages/read-diretory/src/index-ins.js --mode="file" --find-in-dir="D:\\code-store\\nodejs" --regexp="\/.*bd.*.js$/ig"
+// node --no-warnings --loader ./scr/lib/esm-loader.js packages/read-diretory/src/index-ins.js --mode="file_text" --find-in-dir="D:\\code-store\\nodejs" --regexp='\/.js$/ig' --file-text-regexp="\/i love you/ig" --excludes-regexp="\/.zip$/ig"
+// node --no-warnings --loader ./scr/lib/esm-loader.js packages/read-diretory/src/index-ins.js --mode="file_text" --find-in-dir="D:\\code-store\\nodejs" --regexp='\/.js$/ig' --file-text-regexp="\/md/ig"

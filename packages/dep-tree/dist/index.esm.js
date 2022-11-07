b7481ededc20bb4ea8d84f84b0b6caf57e1d1a6d
yemiancheng <ymc.github@gmail.com>
2022-11-07 15:52:29 +0800

    feat(core): parse dep tree
    export class as default
    
    generated by ymc@robot

---
+++ packages/dep-tree/dist/index.esm.js
@@ -0,0 +1,93 @@
+/**
+  * depTree v1.0.0
+  * (c) 2018-2022 ymc
+  * @license MIT
+  */
+import { existsSync } from 'node:fs';
+import ParseHelp from '@ymc/dep-parse';
+
+/* eslint-disable prefer-const,no-unused-vars */
+
+class DepTreeHelp extends ParseHelp {
+  /**
+   * get his dep tree for loc
+   * @param {*} loc
+   */
+  async getTree(loc) {
+    const ph = new ParseHelp();
+    const matchs = await ph.parse(loc);
+    // localdep-only-exsits
+    const localdepex = matchs.filter(dep => {
+      if (/^@/.test(dep)) {
+        return false
+      }
+      return existsSync(dep)
+    });
+
+    // log(`[info] local dep - exsits`);
+    // log(localdepex);
+
+    // dep-tree
+    const deptree = {};
+    deptree[loc] = matchs;
+    const prs = localdepex.map(async match => {
+      if (!deptree[match]) {
+        const res = await ph.parse(match);
+        deptree[match] = res;
+      }
+    });
+    await Promise.all(prs);
+    this.deptree = deptree;
+    // return deptree;
+  }
+
+  circular() {
+    const { deptree } = this;
+    const res = [];
+    const names = Object.keys(deptree);
+    names.forEach(name => {
+      const matchs = deptree[name];
+      if (matchs.some(dep => dep === name)) {
+        res.push(name);
+      }
+    });
+    return res
+  }
+
+  leaves() {
+    const { deptree } = this;
+    const res = [];
+    const names = Object.keys(deptree);
+    let alldeps = names.map(v => deptree[v]);
+    alldeps.push(names);
+    alldeps = alldeps.flat(1);
+    alldeps.forEach(name => {
+      const matchs = deptree[name];
+      // has no dep
+      if (!matchs || matchs.length === 0) {
+        res.push(name);
+      }
+    });
+    return res
+  }
+
+  orphans() {
+    const { deptree } = this;
+    const res = [];
+    const names = Object.keys(deptree);
+    let alldeps = names.map(v => deptree[v]);
+    alldeps.push(names);
+    alldeps = alldeps.flat(1);
+    alldeps.forEach(name => {
+      const matchs = deptree[name];
+      if (!matchs || matchs.length === 0) return
+      // no one is depending on
+      if (!matchs.some(dep => dep === name)) {
+        res.push(name);
+      }
+    });
+    return res
+  }
+}
+
+export { DepTreeHelp as default };
---
+++ packages/dep-tree/readme.md
@@ -0,0 +1,49 @@
+<p align="center" style="background:white;">
+<!-- github workflow stat:s -->
+<!-- one line and center  -->
+  <a href="https://github.com/YMC-GitHub">
+    <img alt="workflow setup" src="https://img.shields.io/static/v1?label=pkg&message=done&color=ff69b4&style=flat-square" />
+  </a>
+  <a href="https://github.com/YMC-GitHub">
+    <img alt="workflow setup" src="https://img.shields.io/static/v1?label=cod&message=done&color=ff69b4&style=flat-square" />
+  </a>
+    <a href="https://github.com/YMC-GitHub">
+    <img alt="workflow setup" src="https://img.shields.io/static/v1?label=dep&message=done&color=ff69b4&style=flat-square" />
+  </a>
+  <a href="https://github.com/YMC-GitHub">
+    <img alt="workflow setup" src="https://img.shields.io/static/v1?label=lin&message=passing&color=ff69b4&style=flat-square" />
+  </a>
+    <a href="https://github.com/YMC-GitHub">
+    <img alt="workflow setup" src="https://img.shields.io/static/v1?label=tes&message={tes_state}&color=ff69b4&style=flat-square" />
+  </a>
+      <a href="https://github.com/YMC-GitHub">
+    <img alt="workflow setup" src="https://img.shields.io/static/v1?label=pro&message=done&color=ff69b4&style=flat-square" />
+  </a>
+
+
+  <!-- https://img.shields.io/badge/<LABEL>-<MESSAGE>-<COLOR> -->
+  <!-- https://img.shields.io/static/v1?label=<LABEL>&message=<MESSAGE>&color=<COLOR> -->
+<!-- github workflow stat:e -->
+</p>
+
+# pkg @ymc/dep-tree
+
+## desc
+parse tree , get circular , get leaves , get orphans and other
+
+## lib size  
+file | size | gzip | brotli
+:---- | :---- | :---- | :----
+dist/index.cjs.js | 15.20kb | 4.19kb | 3.76kb
+dist/index.cjs.min.js | 4.73kb | 1.80kb | 1.64kb
+dist/index.esm.js | 2.06kb | 0.68kb | 0.61kb
+dist/index.esm.min.js | 0.83kb | 0.43kb | 0.38kb
+dist/index.js | 2.06kb | 0.68kb | 0.61kb
+dist/index.umd.js | 16.74kb | 4.37kb | 3.92kb
+dist/index.umd.min.js | 4.98kb | 1.91kb | 1.74kb
+
+## Author
+yemiancheng <ymc.github@gmail.com>
+
+## License
+MIT

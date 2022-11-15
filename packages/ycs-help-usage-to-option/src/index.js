685d6c017de2add3e16ea38526a258f0b234e2e9
yemiancheng <ymc.github@gmail.com>
2022-11-15 22:35:53 +0800

    style(core): lin code
    to keep zero error,warn
    to keep package.json to be not-modified
    
    generated by ymc@robot

--- packages/ycs-help-usage-to-option/src/index.js
+++ packages/ycs-help-usage-to-option/src/index.js
@@ -3,26 +3,26 @@
 // get subns
 // get subcmd
 const getTxtFromUsage = (s, usage = '') => {
-  const regexp = new RegExp(` *${s}:.*`, 'ig')
-  const match = usage.match(regexp)
-  if (match) {
-    return match[0].replace(new RegExp(` *${s}:`, 'i'), '')
-  }
-  return ''
+    const regexp = new RegExp(` *${s}:.*`, 'ig')
+    const match = usage.match(regexp)
+    if (match) {
+        return match[0].replace(new RegExp(` *${s}:`, 'i'), '')
+    }
+    return ''
 }
 
 const genOptionFromUsage = (ns = 'npm-bin', version = '1.0.0', usage = '') => {
-  let option = {}
-  option = {
-    ...option,
-    ...{
-      version,
-      ns,
-      autoSubCmd: getTxtFromUsage('subcmd', usage),
-      autoSubNs: getTxtFromUsage('subns', usage)
+    let option = {}
+    option = {
+        ...option,
+        ...{
+            version,
+            ns,
+            autoSubCmd: getTxtFromUsage('subcmd', usage),
+            autoSubNs: getTxtFromUsage('subns', usage)
+        }
     }
-  }
-  return option
+    return option
 }
 
 export { genOptionFromUsage as usageToOption, getTxtFromUsage }

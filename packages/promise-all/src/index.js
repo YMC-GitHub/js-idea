82f2cadef6fc2cb2d83b7b2da13165300d604964
yemiancheng <ymc.github@gmail.com>
2022-11-25 15:38:23 +0800

    style(core): lin code
    to keep zero error,warn
    to keep package.json to be not-modified
    
    generated by ymc@robot

--- packages/promise-all/src/index.js
+++ packages/promise-all/src/index.js
@@ -14,63 +14,63 @@ import { kindOf } from '@ymc/kind-of'
  * @returns {Promise[]}
  */
 function promiseAll(all, max) {
-  return new Promise((resolve, reject) => {
-    // 'reject' is defined but never used  no-unused-vars
-    const num = max || all.length
-    const lah = new LimitAsyncHandle(num)
+    return new Promise((resolve, reject) => {
+        // 'reject' is defined but never used  no-unused-vars
+        const num = max || all.length
+        const lah = new LimitAsyncHandle(num)
 
-    const data = []
-    // limit max , create task , run task
-    const storeHandleResult = i => {
-      let promise = all[i]
-      // feat: all is async functions
-      if (kindOf(promise) === 'asyncfunction') {
-        promise = promise()
-      } else if (kindOf(promise) === 'function') {
-        // feat: all is functions
-        promise = Promise.resolve(promise())
-      }
-      return promise
-        .then(
-          res => {
-            if (lah._debug) {
-              data[i] = { index: i, data: res, state: 'ok' }
-            } else {
-              data[i] = res
+        const data = []
+        // limit max , create task , run task
+        const storeHandleResult = i => {
+            let promise = all[i]
+            // feat: all is async functions
+            if (kindOf(promise) === 'asyncfunction') {
+                promise = promise()
+            } else if (kindOf(promise) === 'function') {
+                // feat: all is functions
+                promise = Promise.resolve(promise())
             }
-          },
-          error => {
-            if (lah._debug) {
-              data[i] = { index: i, state: 'no', error }
-            } else {
-              data[i] = error
-            }
-          }
-        )
-        .finally(() => {
-          // promise has been settled
-          // if (!this._taskQueue.length && this._count===1) {
-          //     resolve(data);
-          // }
-          // or:
-          lah._cb = () => {
-            //! this._taskQueue.length && this._count===0
-            if (lah._count === 0) {
-              resolve(data)
-            }
-          }
-        })
-    }
+            return promise
+                .then(
+                    res => {
+                        if (lah._debug) {
+                            data[i] = { index: i, data: res, state: 'ok' }
+                        } else {
+                            data[i] = res
+                        }
+                    },
+                    error => {
+                        if (lah._debug) {
+                            data[i] = { index: i, state: 'no', error }
+                        } else {
+                            data[i] = error
+                        }
+                    }
+                )
+                .finally(() => {
+                    // promise has been settled
+                    // if (!this._taskQueue.length && this._count===1) {
+                    //     resolve(data);
+                    // }
+                    // or:
+                    lah._cb = () => {
+                        //! this._taskQueue.length && this._count===0
+                        if (lah._count === 0) {
+                            resolve(data)
+                        }
+                    }
+                })
+        }
 
-    for (let i = 0; i < all.length; i += 1) {
-      // call handle storeHandleResult with limit number
-      lah.call(storeHandleResult, i)
-    }
-    // or:
-    // all.forEach((v,i)=>{
-    //     lah.call(storeHandleResult, i);
-    // })
-  })
+        for (let i = 0; i < all.length; i += 1) {
+            // call handle storeHandleResult with limit number
+            lah.call(storeHandleResult, i)
+        }
+        // or:
+        // all.forEach((v,i)=>{
+        //     lah.call(storeHandleResult, i);
+        // })
+    })
 }
 
 export default promiseAll

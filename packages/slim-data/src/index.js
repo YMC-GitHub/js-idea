9014db2beb21d62d1a35a4126baf1857aa60aecb
yemiancheng <ymc.github@gmail.com>
2022-11-25 15:38:25 +0800

    style(core): lin code
    to keep zero error,warn
    to keep package.json to be not-modified
    
    generated by ymc@robot

--- packages/slim-data/src/index.js
+++ packages/slim-data/src/index.js
@@ -9,45 +9,45 @@
  * @returns {{}[]}
  */
 function slimDataByKeys(data, option) {
-  const opts = {
-    span: ';',
-    excludeKeys: '',
-    ...option
-  }
+    const opts = {
+        span: ';',
+        excludeKeys: '',
+        ...option
+    }
 
-  let list = opts.keys
-  list = isString(list) ? list.split(opts.span) : list
+    let list = opts.keys
+    list = isString(list) ? list.split(opts.span) : list
 
-  let excludeList = opts.excludeKeys
-  excludeList = isString(excludeList) ? excludeList.split(opts.span) : excludeList
-  list = list.filter(v => !excludeList.some(ex => ex === v))
+    let excludeList = opts.excludeKeys
+    excludeList = isString(excludeList) ? excludeList.split(opts.span) : excludeList
+    list = list.filter(v => !excludeList.some(ex => ex === v))
 
-  let res
-  let isOne
-  let cache = Array.isArray(data) ? data : [data]
-  if (Array.isArray(data)) {
-    cache = data
-  } else {
-    cache = [data]
-    isOne = true
-  }
+    let res
+    let isOne
+    let cache = Array.isArray(data) ? data : [data]
+    if (Array.isArray(data)) {
+        cache = data
+    } else {
+        cache = [data]
+        isOne = true
+    }
 
-  res = cache.map(item => {
-    const slim = {}
-    list.forEach(key => {
-      if (key in item) {
-        slim[key] = item[key]
-      }
+    res = cache.map(item => {
+        const slim = {}
+        list.forEach(key => {
+            if (key in item) {
+                slim[key] = item[key]
+            }
+        })
+        return slim
     })
-    return slim
-  })
 
-  // fix Use array destructuring                    prefer-destructuring
-  if (isOne) [res] = res
-  return res
+    // fix Use array destructuring                    prefer-destructuring
+    if (isOne) [res] = res
+    return res
 }
 function isString(s) {
-  return typeof s === 'string'
+    return typeof s === 'string'
 }
 
 export default slimDataByKeys

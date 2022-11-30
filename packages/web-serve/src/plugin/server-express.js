954bade08e860d402ec5e05fa03ebb9825257621
yemiancheng <ymc.github@gmail.com>
2022-11-30 12:57:07 +0800

    style(core): lin code
    to keep zero error,warn
    to keep package.json to be not-modified
    
    generated by ymc@robot

--- packages/web-serve/src/plugin/server-express.js
+++ packages/web-serve/src/plugin/server-express.js
@@ -1,10 +1,15 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable max-len */
 import express from 'express'
+// import favicon from 'serve-favicon'
+
 import config from '../config/server'
 import { makeHttpServer, makeHttpsServer } from './server-helps'
 
 const app = express()
 makeHttpServer(config.pro, app)
 makeHttpsServer(config.pro, app)
+// app.use(favicon())
 
 app.get('/:name', (req, res) => {
     if (req.protocol === 'https') {
